789658b6d23e5c73a733363a3c7472cbcb905824
Will <willadelinephillips@gmail.com>
2021-09-08 13:41:35 -0700

    Additional Pages Made

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Aboutme from "./Aboutme";
  
 class Main extends Component {
   render() {
