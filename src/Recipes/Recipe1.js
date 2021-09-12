6bed5b3cc9c37b0966e35e26332e191628e3e0f9
Will <willadelinephillips@gmail.com>
2021-09-12 15:58:19 -0700

    Ingredients list added

--- src/Recipes/Recipe1.js
+++ src/Recipes/Recipe1.js
@@ -2,17 +2,25 @@ import React, { Component } from "react";
 import {
   NavLink,
 } from "react-router-dom";
+import lemon1 from "../img/lemonylemoncake1.jpg"
+import "../index.css";
  
 class Recipe1 extends Component {
   componentDidMount() {
-    document.title = "Recipe 1";
+    document.title = "Recipe 2";
   }
   render() {
     return (
-      <div class="recipe">
-        <h2>Recipe One</h2>
+      <div id="recipe">
+        <h2>Lemony Lemon Cake</h2>
         <h3>September X, 2021 By <NavLink to="/aboutme">Jillian</NavLink> X Comments</h3>
-        <p>Welcome</p>
+        <p>I am just absolutely enthralled by this darling, delicious lemon cake! From the
+          frosting, to the lemon zest in the batter, there's nothing about this recipe that doesn't
+          just shout "fall delight". I added some little decorative lemons on the top there, but you
+           can decorate it however you like. 
+        </p>
+        <img src={lemon1} width="60%" alt="lemon cake 1"></img>
+
       </div>
     );
   }
