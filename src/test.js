33aa94d15d1f5659e992e7e1bd3fd87588908b4c
Will <willadelinephillips@gmail.com>
2021-09-26 12:16:08 -0700

    Test page content removed

--- src/test.js
+++ src/test.js
@@ -3,18 +3,13 @@ import {
   useRecoilValue,
 } from 'recoil';
 import React from 'react';
-import useSound from 'use-sound';
-import laugh from "./Audio/laugh.mp3"
 
 export function TestDisplay() {
   const darkness = useRecoilValue(Corruption);
-  
-    const [play] = useSound(laugh);
 
       return (
         <p>
           {darkness}%
-          <button onClick={play}>Heh!</button>
         </p>
       );
 }
