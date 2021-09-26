76da098b209f7eddd67d03d9d94e945a7f34ba42
Will <willadelinephillips@gmail.com>
2021-09-26 11:09:48 -0700

    Code unbroken jesus christ

--- src/recoil/corruptionButton.js
+++ src/recoil/corruptionButton.js
@@ -3,7 +3,8 @@ import {
   } from 'recoil';
   
 import { Corruption } from './atoms';
-  
+import React from 'react';
+
 export function CorruptionButton() {
     const [darkness, setDarkness] = useRecoilState(Corruption);
       return (
--- src/test.js
+++ src/test.js
@@ -2,6 +2,7 @@ import { Corruption } from './Recoil/atoms';
 import {
   useRecoilValue,
 } from 'recoil';
+import React from 'react';
 
 export function TestDisplay() {
   const darkness = useRecoilValue(Corruption);
