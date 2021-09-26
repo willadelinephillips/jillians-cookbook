4e148ade9ee76965394bc25d05ba69b546c4aa54
Will <willadelinephillips@gmail.com>
2021-09-26 10:32:14 -0700

    Debug homepage described

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -24,12 +24,10 @@ class Home extends Component {
     return (
       <div>
         <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
+        <p>Debug page for Jillian's Cookbook. Text is added here to see the full extent of the page. 
+          Below are samples of working additions that can be controlled across the entire website.
+          
+        </p>
  
         <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
 
