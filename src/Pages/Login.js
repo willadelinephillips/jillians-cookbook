ac904e2578b164838669d52a0faf7a8cae07744e
Will <willadelinephillips@gmail.com>
2021-09-29 16:24:53 -0700

    Username and password forms working

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,12 +1,17 @@
 import React from "react";
 import {
   useRecoilState,
+  useRecoilValue
 } from 'recoil';
-import { hideQuestions } from '../Recoil/atoms';
+import { hideQuestions, potentialpass, potentialuser, username, password } from '../Recoil/atoms';
  
 export function Login() {
     document.title = "Login";
 
+    const [passattempt, setPassAttempt] = useRecoilState(potentialpass);
+    const [userattempt, setUserAttempt] = useRecoilState(potentialuser);
+    const user = useRecoilValue(username);
+    const pass = useRecoilValue(password);
     const [hideQs, setHideQs] = useRecoilState(hideQuestions);
 
     return (
@@ -14,14 +19,16 @@ export function Login() {
         <h2>LOGIN</h2>
         <form action="/action_page.php">
           <label for="username" id="usertitle">USERNAME: </label>
-          <input type="text" id="username" name="username"></input>
+          <input type="text" id="username" name="username" onChange={(event) => setUserAttempt(event.target.value)}></input>
+
           <label for="password" id="userpass">PASSWORD: </label>
-          <input type="text" id="password" name="password"></input>
+          <input type="text" id="password" name="password" onChange={(event) => setPassAttempt(event.target.value)}></input>
         </form>
         <input type="submit" value="Submit"></input>
         <input type="checkbox" id="rememberme" name="rememberme" value="Yes"></input>
 <label for="rememberme"> Remember Me</label>
         <button onClick={() => setHideQs('revealed')}>Forgot Password?</button>
+        Tester: {userattempt}, {passattempt}
         <div className="securityQuestions">
         <p id="hintsection" className={`${hideQs}`}>
           In order to sign in once your password is forgotten, you must answer your pre-written 
