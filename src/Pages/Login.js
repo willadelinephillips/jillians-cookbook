7aac9fb8c917ec052ea3bf790ae2bb4f8a58a561
Will <willadelinephillips@gmail.com>
2021-09-28 19:37:08 -0700

    Signin styled

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -11,12 +11,12 @@ class Login extends Component {
         <p>
         </p>
         <form action="/action_page.php">
-          <label for="username">USERNAME: </label>
+          <label for="username" id="usertitle">USERNAME: </label>
           <input type="text" id="username" name="username"></input>
-          <label for="password">PASSWORD: </label>
+          <label for="password" id="userpass">PASSWORD: </label>
           <input type="text" id="password" name="password"></input>
-          <input type="submit" value="Submit"></input>
         </form>
+        <input type="submit" value="Submit"></input>
         <input type="checkbox" id="rememberme" name="rememberme" value="Yes"></input>
 <label for="rememberme"> Remember Me</label>
         <button>Forgot Password?</button>
