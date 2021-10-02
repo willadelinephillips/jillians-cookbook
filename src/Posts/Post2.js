892164ea0bf109fdb85e1b18a0b78e57300a786b
Will <willadelinephillips@gmail.com>
2021-10-01 18:37:32 -0700

    Started second post

--- src/Posts/Post2.js
+++ src/Posts/Post2.js
@@ -14,7 +14,17 @@ class Post2 extends Component {
         <h2>Post 2</h2>
         <h3>September X, 2021 By <NavLink to="/aboutme">Jillian</NavLink> 0 Comments</h3>
         <p>
-            Lorum Ipsum
+            I know I shouldn't be using this posting system as my own private diary, but for now no 
+            one can read them and I need somewhere to vent. 
+        </p>
+        <p>
+            I had no idea how much this would all be! I've been so overworked and underappreciated. 
+            ALthough I know that my friends are trying to be supportive, they come off as 
+            condescending and annoyed much of the time. My recipes feel like they fall flat, I'm 
+            cooking all the time, and I hardly ever leave the house with how busy I've made myself. 
+        </p>
+        <p>
+            
         </p>
 
       </div>
