362ad6f92d045bb03ce27a159988df50cdf26cb2
Will <willadelinephillips@gmail.com>
2021-10-02 16:57:39 -0700

    Post updated

--- src/Posts/Post3.js
+++ src/Posts/Post3.js
@@ -14,7 +14,10 @@ class Post3 extends Component {
         <h2>Post 3</h2>
         <h3>September X, 2021 By <NavLink to="/aboutme">Jillian</NavLink> 0 Comments</h3>
         <p>
-            Last time with the ranting to the website who can't hear me. 
+            Last time with the ranting to the website who can't hear me. At least. I hope. Okay, I 
+            know how that sounds. And you gotta hear me out here, if anyone ever finds this post 
+            (because let's be honest, even if I figure out how to post this where someone can see 
+            it, I doubt the website will let me.)
         </p>
 
       </div>
