914fdb67c94d6e1a72d3a28a1036d8899cbca350
Will <willadelinephillips@gmail.com>
2021-10-03 16:27:02 -0700

    Recipe2 update

--- src/Recipes/Recipe2.js
+++ src/Recipes/Recipe2.js
@@ -5,12 +5,15 @@ import {
 
 
 //Image Imports 
-import lemon1 from "../img/lemonylemoncake1.jpg"
-import lemon2 from "../img/lemonylemoncake2.jpg"
-import lemon3 from '../img/lemonylemoncake3.jpg'
-import lemon4 from "../img/lemonylemoncake4.jpg"
-import randomavatar from "../img/randomavatar.png"
-
+import {
+  karenpfp,
+  sarahjeanpfp,
+  carreypfp,
+  lemonylemoncake1,
+  lemonylemoncake2,
+  lemonylemoncake3,
+  lemonylemoncake4
+} from "../img/index"
 //Style import
 import "../index.css";
  
@@ -29,26 +32,26 @@ class Recipe2 extends Component {
           just shout "fall delight". I added some little decorative lemons on the top there, but you
            can decorate it however you like. 
         </p>
-        <img src={lemon1} width="60%" alt="lemon cake 1"></img>
+        <img src={lemonylemoncake1} width="60%" alt="lemon cake 1"></img>
         <p>My mother would make this recipe for us on hot summer days near the start of fall, when
           the leaves were just beginning to drop from the trees. We would come home to the smell of
            lemon zest and a hint of vanilla, the perfect respite after sweltering hot hours in a 
            classroom with no air conditioning. When I bake this, I hear her laughter. 
         </p>
-        <img src={lemon2} width="60%" alt="lemon cake 2"></img>
+        <img src={lemonylemoncake2} width="60%" alt="lemon cake 2"></img>
         <p>I made these little lemons out of balls of frosting with a little mint leaf on top from 
           my garden. Although cute and traditional, they're optional and the cake is perfectly 
           delicious without them. I may decide to do an entire recipe on how to make little frosting 
           decorations, but for now that's very dependant on the comments I receive on this post! If 
           you want to see that in the future, let me know.
         </p>
-        <img src={lemon3} width="60%" alt="lemon cake 3"></img>
+        <img src={lemonylemoncake3} width="60%" alt="lemon cake 3"></img>
         <p>The crust is made of graham crackers and butter, which is the secret to the cake. I wont be 
           revealing which particular brand I use here as I've been sworn to secrecy by my mother years 
           ago, but all brands work just fine. If you find a brand that worked especially well, let me 
           know!
         </p>
-        <img src={lemon4} width="60%" alt="lemon cake 3"></img>
+        <img src={lemonylemoncake4} width="60%" alt="lemon cake 3"></img>
         <p>My own personal trick with mixing the batter is always patience. Don't rush it or go too 
           fast, or you'll miss lumps and bits of dry ingredients. I know that's what everyone says, 
           but there's really an art to stirring as slowly as possible. Maybe put on some music, 
@@ -97,19 +100,19 @@ class Recipe2 extends Component {
           <ul className="commentSection">
 
             <li>
-              <img src={randomavatar}alt="random avatar"></img>
+              <img src={sarahjeanpfp}alt="sarahjean"></img>
               <p className="username">Sarah Jean - September X, 2021 :</p>
               <p>Love this recipe, dear! So excited to see you following your dreams!</p>
             </li>
 
             <li>
-              <img src={randomavatar}alt="random avatar"></img>
+              <img src={carreypfp}alt="carrey"></img>
               <p className="username">Carrey - September X, 2021 :</p>
               <p>Sounds delicious, can't wait to try it. I love those little decorations!</p>
             </li>
 
             <li>
-              <img src={randomavatar}alt="random avatar"></img>
+              <img src={karenpfp}alt="karenrider"></img>
               <p className="username">Karen Rider - September X, 2021 :</p>
               <p>Looks just like the one from that book we read last week at club. Inspiration maybe?</p>
             </li>
