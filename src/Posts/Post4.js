ac0bb393884e03e63e7e8abca55bf8fc9708d44d
Will <willadelinephillips@gmail.com>
2021-10-04 08:15:33 -0700

    Post 4 written

--- src/Posts/Post4.js
+++ src/Posts/Post4.js
@@ -11,12 +11,24 @@ class Post4 extends Component {
   render() {
     return (
       <div id="post">
-        <h2>Post 4</h2>
-        <h3>September X, 2021 By <NavLink to="/aboutme">Jillian</NavLink> 0 Comments</h3>
+        <h2>H̴̞͉̓̐̌̾̒̀̿͝Ę̸͍̭͖͙̙̹̮̝͔̲̰͕̹̝̈́̂L̴̢̨̧͉̻̳̲̝̖͎̯̬͖̻̦̏̅̀̃̈́P̴̢̧̰̰̝̪͎͎̖̦̞̣͉̤͊͗́</h2>
+        <h3>????????? ?, ???? By <NavLink to="/aboutme">Jillian</NavLink> 0 Comments</h3>
         <p>
-            Lorum Ipsum 4
+            It's so dark. I can't see anything. Sometimes I can see the recipes, or I can see into 
+            my kitchen. But the rest is all black, like it's not even there. There's this constant 
+            humming coming from the dark, and this awful slithering, skittering noise. Y'know the 
+            noise that centipedes make when they crawl across the ground, scattering dead leaves 
+            and debris? It sounds like that. Like something with more legs than anything has the 
+            right to have is looking, hunting. Hungry.
         </p>
-
+        <p>
+            I don't remember the last time I felt like I was in my house. This dark feeling has 
+            been taking my memories, slowing down my mind. I know that I should do things, like 
+            call my dad or go outside, but somehow I... can't. Like I'm trapped here. This feeling 
+            reminds me of the panic attacks I was having when Mom first passed, but I can't ground 
+            myself. I keep losing count. Something I can see, something I can... touch?
+        </p>
+        <h3>I can't feel my face. I can't feel my hands. I can't feel my body. Where am I?</h3>
       </div>
     );
   }
