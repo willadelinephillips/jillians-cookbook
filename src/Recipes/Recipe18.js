57672ab71d9bbf1e5466d55ff92d33d137a458bc
Will <willadelinephillips@gmail.com>
2021-10-05 15:44:59 -0700

    Recipe 18 done

--- src/Recipes/Recipe18.js
+++ src/Recipes/Recipe18.js
@@ -4,123 +4,123 @@ import {
 } from "react-router-dom";
 import "../index.css";
 
-import { randomavatar } from "../img/index";
+import { humanity, soul, self, ancient, cauldron } from "../img/index";
  
 function Recipe18() {
 
-    document.title = "Recipe 18";
+    document.title = "άνθρωπος";
 
     return (
       <div>
         <div id="recipe">
-        <h2>Recipe Name</h2>
-        <h3>September X, 2021 By <NavLink to="/aboutme">Jillian</NavLink>. 0 Comments ↓</h3>
+        <h2>άνθρωπος</h2>
+        <h3>Σεπτέμβριος, 2021 <NavLink to="/aboutme">θύμα</NavLink>. μηνύματα ↓</h3>
         <p> 
-          This is some kind of intro paragraph, get people invested and interested in what you're 
-          cooking. Y'know, like 'this is such a nice season to be cooking or something. I don't 
-          know. Maybe talk about where you got the idea for writing the recipe. It needs to take 
-          up a lot of space like the other paragraphs, so you really gotta stretch what you're 
-          putting in here. Talk about what gave you the idea to write this recipe in the first place, 
-          or talk about where you got it from if it's not exactly 100% yours. If it's one of Mom's 
-          recipes or something, make sure to put a good word in about her here. Remember, you're 
-          supposed to be coping in a healthy way, and making solid attempts to talk about her in a 
-          positive way instead of pretending she's not gone is part of the healing process.
+          Θα καταβροχθίσουμε τους ζωντανούς για να διατηρήσουμε όχι μόνο τη δική μας ζωή, αλλά και να καταναλώσουμε
+          τη δύναμη που περιείχε ο άνθρωπος μέσα του. Οι άνθρωποι είναι γεμάτοι δύναμη ζωής, ενέργεια
+          που συνδέει όλα τα έμβια όντα με το θνητό επίπεδο. Όταν καταναλώνουμε έναν άνθρωπο, εμείς
+          πάρουμε αυτή τη δύναμη και την τραβάμε στον εαυτό μας, σκίζοντας την άχρηστη σακούλα τους με κομμάτια
+          στην πορεία ώστε να μην υπάρχουν υπολείμματα. Όσο πιο βίαιος και τρομερός είναι ο θάνατος
+          για τον άνθρωπο, τόσο πιο νόστιμη και δυνατή η δύναμη της ζωής. Δεν ξέρουμε γιατί
+          Αυτό είναι, γνωρίζουμε μόνο ότι αυτός είναι ο καλύτερος τρόπος για να τρώμε δύναμη ζωής.
         </p>
-        <img width="10%" alt="IMG 1"></img>
+        <img width="50%" src={humanity} alt="IMG 1"></img>
         <p>
-          Throw some sentimental nonsense in here. If you have absolutely no connection to the 
-          recipe on a personal level, make one up. Be like "god I remember that one time I ate this 
-          at that one place" or something. Let's be honest, no one will ever read most of this so 
-           there's no reason to write a masterpiece in these paragraphs. I wish people would read 
-           it, but I just know they'll scroll right down and scan the ingredients. Honestly, these 
-           are just little filler thingies to make the pages look more professional. I could just 
-           write a bunch of random words here and no one would care.
-        </p>
-        <img width="10%" alt="IMG 2"></img>
+          Ο άνθρωπος είναι η ρίζα κάθε κακού. Τους παρουσιάζονται ευκαιρίες να είναι καλά πλάσματα, 
+          πλάσματα που δίνουν έλεος και αγάπη σε πράγματα μικρότερα και πιο αδύναμα από τον εαυτό 
+          τους. Αυτός είναι και ο λόγος που είναι τόσο νόστιμα. Θεωρούν ότι οι ευκαιρίες είναι κακά, 
+          κακά, καταστροφικά πλάσματα που δεν σκέφτονται τίποτα άλλο παρά τον εαυτό τους και αγνοούν 
+          όλες τις ευκαιρίες για να είναι καλά πλάσματα, εκτός εάν αυτές οι ευκαιρίες ωφελήσουν με 
+          κάποιο τρόπο τα άπληστα μυαλά τους. Αυτή η ηθική διαφθορά τους οδηγεί στο να είναι το τέλειο 
+          καντίδα για ιεροσυλία, καθώς σε αντίθεση με άλλα ζώα, κάνουν ελεύθερα την επιλογή να είναι 
+          λεκέδες στη Γη. Πώς θα μπορούσε ένας ελεήμων Θεός να επιτρέψει σε αυτά τα φρικτά πλάσματα να 
+          ζήσουν στη γη Του και να μην ευχαριστεί εμάς που τα απομακρύνουμε και παίρνουμε τη δύναμή τους 
+          για τον αγώνα;
+          </p>
+        <img width="50%" src={soul} alt="IMG 2"></img>
         <p>
-          This is where you want to talk about the actual cooking. How you did it, what little 
-          tricks you used, and what happened. Don't spend this whole paragraph whining about how 
-          hard it was or something, since Karen and the others are reading this and they'll just 
-          comment about how they wouldn't have had those problems. Sure, Karen. Sure you wouldn't 
-          have. You're just perfect, aren't you. So perfect that you're only struggle is seeing the 
-          obnoxious flaws in other flawed, annoying human beings like your fellow club members. 
-          Why can't you be perfect enough to shut up and stop shoving your snide little opinions 
-          where no one wants them or asked for them. 
-        </p>
-        <img width="10%" alt="IMG 3"></img>
-        <p>
-          I guess you could describe this section as like. "Suggestions tips and ideas". I know 
-          that sounds kind of like the section before it, but hey. It's a recipe website. The 
-          website should really just be the ingredients and the instructions. Anything else added 
-          is just some worthless junk to make people think you're some sort of blogging expert and 
-          so you can claim you're one on your resume. I would put stuff like "how to cook with 
-          x ingredient" or "which brand of this is the best in my profession (what a joke) opinion". 
-          If you have absolutely no idea what to put here, just make up some ideas of how to decorate 
-          what ever it is the recipe is about. That way it looks like you had something to say.
-        </p>
-        <img width="10%" alt="IMG 4"></img>
-        <p>
-          Storing and reheating the recipe! Storage containers picture combined with some tips 
-          about how to do really basic stuff that everyone's mom should have taught them to do when 
-          they were 12, like "don't eat leftovers that have been in the fridge for more than two 
-          weeks" or something to that idea. How it tasted when I had it reheated, which will be a 
-          half-truth depending on how good it actually tasted. If it tasted like freshly minced 
-          garbage or something, just say that it doesn't keep especially well. You can also talk 
-          about how to make whatever it is ahead of time, although sometimes that's a really 
-          terrible idea. 
-        </p>
-        <img width="10%" alt="IMG 5"></img>
-        <p>
-          Finally, this is the "oh, all the occasions you'll be using this for" section. Like the 
-          reader can't figure out on their own which occasion to bake cupcakes. Sometimes I read 
-          the other cooking blog/recipe websites and I wonder if the author was completely sane 
-          writing all that they did. Maybe the trick is to just write like the reader is a small 
-          toddler incapable of rational thought. It feels like that's what they're doing sometimes. 
-          This is the reality of trying to live your dream though. You have to not just accept the 
-          parts that are beautiful and fun, but the annoying or ugly parts. That's reality.  
-        </p>
-        <h3>Ingredients</h3>
-        <p>Main Recipe ingredients</p>
-        <ul>
-          <li>A single ingredient</li>
-          <li>A single ingredient</li>
-          <li>A single ingredient</li>
-          <li>A single ingredient</li>
-          <li>A single ingredient</li>
-        </ul>
-        <p>Secondary Recipe ingredients</p>
-        <ul>
-          <li>Another ingredient</li>
-          <li>Another ingredient</li>
-          <li>Another ingredient</li>
-        </ul>
-        <p>Instructions</p>
-        <p>First, these are your first instructions. Preheating the oven, doing your mise en place, 
-          that sort of thing. Cutting, chopping, stirring, separating the dry and wet ingredients. 
 
+          Το πρώτο βήμα για να καταβροχθίσει την ψυχή ενός ανθρώπου είναι να την διαλύσει ψυχικά. 
+          Δεν εγκαταλείπουν την ψυχή τους τόσο εύκολα, είναι σαν να μαγειρεύεις ένα ψητό. Χρειάζεται 
+          χρόνος και υπομονή. Τους δείχνεις πράγματα που δεν υπάρχουν, παίρνεις τα όνειρά τους και τα 
+          παρασύρεις σε κακά πράγματα που τους κάνουν να αισθάνονται σαν να χάνουν το μυαλό τους. 
+          Πρέπει να βεβαιωθείτε ότι είναι εντελώς απομονωμένοι από τους φίλους και την οικογένειά 
+          τους, η καλύτερη ανθρώπινη θυσία είναι ένας άνθρωπος που έχει βγάλει τον εαυτό του από την 
+          ομάδα των άλλων ανθρώπων και είναι μόνος και φοβάται. Ο φόβος και ο θυμός είναι καλά 
+          υναισθήματα για να κάνουν τον άνθρωπο να νιώσει, κάνουν τη δύναμη της ζωής πιο ισχυρή. 
+          Μόλις φοβούνται και θυμώνουν σωστά, τότε πρέπει να τους κάνετε να καταστρέψουν τον εαυτό 
+          τους. Πετάξτε από μια δομή. Πολεμήστε σε μια μονομαχία δεν μπορούν να κερδίσουν. Το 
+          αγαπημένο μου είναι να τους λέω να γίνουν κομμάτια με τα χέρια τους. Χωρίς εργαλεία, χωρίς 
+          όπλα. Μόνο δόντια και νύχια και σάρκα και πόνος.
         </p>
-        <p>Second, these are the second set of instructions. 
+        <img width="50%" src={self} alt="IMG 3"></img>
+        <p>
+        Είμαι η πείνα. Είμαι η αρχή και το τέλος των καιρών. με λες λαιμαργία, με λες την 
+        πεινασμένη. Κάθε ένα από τα αδύναμα σαρκικά πλάσματα με έχει μέσα του, απλώνει την 
+        καρδιά και την ψυχή του και πιάνει το καλό, το νόστιμο, το νόστιμο. Μένω μέσα σου και σε 
+        τρώω από μέσα, δηλητηριάζοντας τις φλέβες σου και καίγοντας το εσωτερικό σου. Αν με αφήσεις 
+        να σε χωρίσω, θα σε πεινάσω τόσο που θα λαχταράς ακόμη και τη σάρκα των δικών σου φίλων, 
+        της δικής σου οικογένειας. Η δική σας σάρκα, η νόστιμη, απαλή κάλυψη που κρύβει τις 
+        αμαρτίες και τις παραβάσεις σας. Θα κάνω το τέλος των εποχών το πιο νόστιμο τέλος που θα 
+        μπορούσε να φανταστεί κανείς. Τα αδέλφια μου μπορούν να ονειρεύονται να κάνουν 
+        πραγματικότητα τους τελευταίους καιρούς, αλλά ξέρω ότι οι αδύναμοι άνθρωποι θα με επιλέξουν 
+        στο τέλος.
         </p>
-        <p>Next, these are your third set of instructions. 
+        <img width="60%" src={ancient} alt="IMG 4"></img>
+        <p>
+        Μου λείπουν οι παλιοί καιροί. Πριν από πολλά χρόνια, τόσα χρόνια που έχασα τον αριθμό, οι 
+        άνθρωποι έρχονταν πρόθυμοι σε εμάς. Θα γονατίσουν μπροστά μας και εναντίον του Θεού και θα 
+        μας δώσουν τη δύναμη της ζωής τους. Wasταν η πιο νόστιμη δύναμη ζωής που μπορείτε να 
+        φανταστείτε, αφού δόθηκε δωρεάν. Τους κάναμε ακόμα να αισθάνονται πόνο, φυσικά, αλλά ήταν 
+        πόνος για τον οποίο είχαν παρακαλέσει και παρακαλούσαν. Το να γνωρίζουμε ότι ήταν πρόθυμοι 
+        να πάνε εναντίον του Θεού για να μας ταΐσουν ήταν το πιο ένδοξο συναίσθημα που μπορεί 
+        κανείς να φανταστεί. Έτσι είναι τώρα. Τώρα ο Θεός που φοβάται τους ανθρώπους μας έχει 
+        φιμώσει για τόσο πολύ καιρό που έχουμε ξεχαστεί, ή ακόμα χειρότερα, μειωμένοι σε αστεία. 
+        Τώρα παρουσιάζουμε ως ανόητα αστεία στην τηλεόραση και στις οθόνες. Μας κοροϊδεύουν και δεν 
+        μπορούμε να κάνουμε τίποτα άλλο από το να καθίσουμε και να παρακολουθούμε από αυτήν τη 
+        φυλακή. Όταν απελευθερωθώ για άλλη μια φορά στον κόσμο, θα βρω τους ανθρώπους που μας 
+        χλεύασαν πρώτα. Θα τα σκίσω σε τόσα κομμάτια που δεν θα τα βρουν ποτέ όλα. Και τότε θα 
+        φέρω τον φόβο που έχουν οι άγιοι για τον Θεό και θα τον στρέψω προς το μέρος μου. Θα ξέρουν 
+        τι σημαίνει να φοβάσαι ένα ισχυρό ον.
         </p>
-        <p>Then, these are your fourth set of instructions. 
+        <img width="50%" src={cauldron} alt="IMG 5"></img>
+        <p>
+        Θα τους βράσω το μυαλό. Θα βράσω τις ψυχές τους. Θα σκίσω και θα τεμαχίσω, θα σκίσω και θα 
+        σχίσω μέχρι να μην μείνει τίποτα. Και θα ξεκινήσω με αυτήν την ανόητη γυναίκα, αυτή την 
+        τυφλή ανόητη που μου επέτρεψε τόσο βαθιά στην καρδιά και την ψυχή της, ώστε να γίνουμε 
+        ένα, ένα ενιαίο πλάσμα που αποτελείται κυρίως από εμένα και το μικρότερο κομμάτι της. 
+        Φωνάζει και κλαίει σε αυτόν τον μεταλλικό κόσμο και δεν υπάρχει κανείς που να τη σώσει. 
+        Θα την κάνω την ελεεινή μικρή που κλαίει να γίνει στιφάδο, και μετά θα την καταπιώ ολόκληρη 
+        και η δύναμη θα μου επιτρέψει να είμαι ξανά ελεύθερη στον ανθρώπινο κόσμο. Θα περπατήσω 
+        ανάμεσά τους με τα σιωπηλά βήματα εκείνου που είναι χωρίς μορφή και στη συνέχεια θα κλέψω 
+        μια δική μου μορφή από τις σαρκώδεις φυλακές και θα τους βγάλω το μυαλό από την ψυχή τους. 
+        Χρησιμοποιώντας αυτό το σώμα θα φάω περισσότερους ανθρώπους, αυτό το μεταλλικό κουτί με 
+        έμαθε πώς να σκοτώνω χωρίς να με πιάσουν οι φρουροί τους. Θα φάω τόσα πολλά. Θα μπω κρυφά 
+        στα σπίτια τους. Ποτέ δεν θα υπάρχει αρκετή πείνα και υπάρχει μια ατελείωτη παροχή ανόητων 
+        γευμάτων που κοιμούνται στα σπίτια τους, ονειρεύονται ότι είναι ασφαλή.
         </p>
-        <p>After that, these are your fifth set of instructions. 
+        <h3>συστατικά</h3>
+        <ul>
+          <li>καρδιά</li>
+          <li>εγκέφαλος</li>
+          <li>έντερα</li>
+          <li>συκώτι</li>
+          <li>στομάχι</li>
+        </ul>
+        <p>να κάνω</p>
+        <p>κάνε τους να τρελαθούν
         </p>
-        <p>Finally, these are your final set of ingredients.
+        <p>σκίζουν την καρδιά τους
         </p>
-        <p id="commentSection">Comments</p>
+        <p>κόβουν το συκώτι τους
+        </p>
+        <p>να συντρίψουν τη σπονδυλική τους στήλη
+        </p>
+        <p>τρώνε το στομάχι τους
+        </p>
+        <p>καταβροχθίζουν την ψυχή τους
+        </p>
         </div>
-
-          <ul className="commentSection">
-
-            <li>
-              <img src={randomavatar}alt="random avatar"></img>
-              <p className="username">Jillian - September X, 2021 :</p>
-              <p>Commenting on here myself to see what the comments will look like!</p>
-            </li>
-          </ul>
-
       </div>
     );
   }
--- src/img/index.js
+++ src/img/index.js
@@ -7,8 +7,13 @@ import lemonylemoncake4 from "./lemonylemoncake4.jpg";
 import karenpfp from "./karenpfp.jpg";
 import sarahjeanpfp from "./sarahjeanpfp.jpg";
 import carreypfp from './carreypfp.jpg';
-import ouroboros from "./ouroboros.png"
-import jilliansicon from "./jilliansicon.png"
+import ouroboros from "./ouroboros.png";
+import jilliansicon from "./jilliansicon.png";
+import humanity from "./humanity.jpg";
+import soul from "./soul.jpg";
+import self from "./self.jpg";
+import ancient from "./ancient.jpg";
+import cauldron from "./cauldron.jpg";
 
 export {
     randomavatar,
@@ -21,5 +26,10 @@ export {
     sarahjeanpfp,
     carreypfp,
     ouroboros,
-    jilliansicon
+    jilliansicon,
+    humanity,
+    soul,
+    self,
+    ancient,
+    cauldron
 }
