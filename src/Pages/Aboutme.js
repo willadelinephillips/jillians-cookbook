e55c1362613c75feb82d133820d564d4176420eb
Will <willadelinephillips@gmail.com>
2021-10-06 18:07:41 -0700

    About Me updated

--- src/Pages/Aboutme.js
+++ src/Pages/Aboutme.js
@@ -1,19 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
  
-class Aboutme extends Component {
-  componentDidMount() {
+function Aboutme() {
+
     document.title = "About Me";
-  }
-  render() {
+
     return (
       <div>
-        <h2>About</h2>
-        <p>Welcome, my name is Jillian! It's been my dream my entire life to have a food website.</p>
-        <br></br>
-        <p>Yeet</p>
+        <h2>About Me</h2>
+        <img width="10%" alt="IMG 1"></img>
+        <p>Welcome, my name is Jillian! It's been my dream my entire life to have a food website. 
+          Even when I was a little girl pretending to cook in my little pastic kitchen, I imagined 
+          an audience that was watching me. I told them about the ingredients I was using, the way 
+          I had decided to maket the recipe, and what personal touches I was adding. When I got 
+          older, it was my way of connecting with those I love. My mother, my father, and my friends. 
+          Our family didn't express a lot of love through words or hugs, but through our cooking 
+          instead. When my mother passed, the primary thing she left me was her cookbook, full of 
+          notes and tips and tricks. It's my most treasured possession.
+        </p>
+        <img width="10%" alt="IMG 2"></img>
+        <p>Thank you for being here, on my website with me. It means a lot to know that my dream 
+          is being realized, no matter if you're a close personal friend or a stranger out here 
+          on the 'Web. Your happiness in these recipes is my happiness in your enjoyment. Please, 
+          leave a comment, send me a picture of your recipe. Show me how much (or how little) it 
+          changed your event or meal. It means the world to hear about how my writing has affected 
+          you, even if it's just a critique. Critiques mean that I learn and get better!
+        </p>
+        <img width="10%" alt="IMG 3"></img>
       </div>
     );
   }
-}
+
  
 export default Aboutme;
